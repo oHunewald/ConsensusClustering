f26e699d0b09d8622e35c05ab315c3134640c476
ohunewald <oliver.hunewald@lih.lu>
2020-01-16 11:02:20 +0100

    implemented function resample

--- src/consensusCluster.jl
+++ src/consensusCluster.jl
@@ -10,3 +10,17 @@ using Clustering
 M = rand(5,5)
 M += M'; # symmetric distance matrix (optional)
 result = hclust(M, linkage=:single)
+
+distance = "euclidean"
+
+
+function resample(data, proportion)
+
+    n = Int(size(data,1) * proportion)
+    s = rand(1:size(data,1), n)
+    return s, data[s,:]
+end
+
+
+randWorkers = rand(1:nworkers(), numCodes)
+    X = zeros(numCodes, nCols)
